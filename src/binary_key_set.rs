8f6dad9441905e537ce5b51418b45fa046af4978
Mark Green <markosindustries@gmail.com>
2019-03-25 17:24:58 +1100

    Reduce space wastage for list-type serialisation

--- src/binary_key_set.rs
+++ src/binary_key_set.rs
@@ -2,10 +2,9 @@ use std::collections::HashMap;
 // use std::collections::bit_vec::BitVec;
 use bitvec::*;
 
-const MAGIC_BYTE_CHILDREN_IS_BITMAP: u8 = 0b1000_0000;
-const MAGIC_BYTE_CHILDREN_IS_LIST: u8   = 0b0100_0000;
-const MAGIC_BYTE_CHILDREN_IS_EMPTY: u8  = 0b0010_0000;
-const MAGIC_BYTE_IS_MATCH: u8           = 0b0000_0001;
+const MAGIC_BYTE_LIST_LENGTH_MASK: u8   = 0b0001_1111;
+const MAGIC_BYTE_CHILDREN_IS_BITMAP: u8 = 0b0010_0000;
+const MAGIC_BYTE_IS_MATCH: u8           = 0b1000_0000;
 
 #[derive(Debug)]
 pub struct BinaryKeySet {
@@ -75,15 +74,16 @@ impl BinaryKeySet {
         let mut child_exists_bytes: Vec<u8> = vec![];
         let magic_byte: u8 = if self.is_match {MAGIC_BYTE_IS_MATCH} else {0};
         let magic_byte = match self.children.len() {
-            0 => magic_byte | MAGIC_BYTE_CHILDREN_IS_EMPTY,
+            0 => magic_byte,
             len if len < 32 => {
-                let mut child_exists_as_vec: Vec<u8> = child_exists.iter().enumerate().filter(|(_byte, exists)| *exists).map(|(byte, _exists)| byte as u8).collect();
-                child_exists_bytes.push(len as u8);
-                child_exists_bytes.append(&mut child_exists_as_vec);
-                magic_byte | MAGIC_BYTE_CHILDREN_IS_LIST
+                child_exists_bytes = child_exists.iter().enumerate()
+                    .filter(|(_byte, exists)| *exists)
+                    .map(|(byte, _exists)| byte as u8)
+                    .collect();
+                magic_byte | (len as u8 & MAGIC_BYTE_LIST_LENGTH_MASK)
             },
             _ => {
-                child_exists_bytes.append(&mut child_exists.as_mut_slice().to_vec());
+                child_exists_bytes = child_exists.as_slice().to_vec();
                 magic_byte | MAGIC_BYTE_CHILDREN_IS_BITMAP
             },
         };
@@ -109,56 +109,41 @@ impl BinaryKeySet {
     }
 
     fn deserialize_next(buf: &[u8]) -> Result<(Self, &[u8]), String> {
-        if let Some((magic_byte, buf)) = buf.split_first() {
-            let mut owned_children = bitvec![BigEndian, u8; 0; 256];
-            let mut next = BinaryKeySet::new();
-            next.is_match = MAGIC_BYTE_IS_MATCH == magic_byte & MAGIC_BYTE_IS_MATCH;
-            match if MAGIC_BYTE_CHILDREN_IS_LIST == magic_byte & MAGIC_BYTE_CHILDREN_IS_LIST {
-                if let Some((length, mut buf)) = buf.split_first() {
-                    let mut error = None;
-                    for _ in 0..*length {
+        match buf.split_first() {
+            None => Err("Not enough bytes".to_string()),
+            Some((magic_byte, mut buf)) => {
+                let mut child_exists = bitvec![BigEndian, u8; 0; 256];
+                let mut next = BinaryKeySet::new();
+                next.is_match = MAGIC_BYTE_IS_MATCH == magic_byte & MAGIC_BYTE_IS_MATCH;
+                if MAGIC_BYTE_CHILDREN_IS_BITMAP == magic_byte & MAGIC_BYTE_CHILDREN_IS_BITMAP {
+                    if buf.len() < 32 {
+                        return Err("Not enough bytes".to_string())
+                    }
+                    child_exists = buf[0..32].into();
+                    buf = &buf[32..];
+                } else /* It's a list */ {
+                    let length = magic_byte & MAGIC_BYTE_LIST_LENGTH_MASK;
+                    for _ in 0..length {
                         if let Some((byte, remaining_buf)) = buf.split_first() {
-                            owned_children.set(*byte as usize, true);
+                            child_exists.set(*byte as usize, true);
                             buf = remaining_buf;
                         } else {
-                            error = Some(Err("Not enough bytes".to_string()));
-                            break;
+                            return Err("Not enough bytes".to_string());
                         }
                     }
-                    match error {
-                        Some(err) => err,
-                        None => Ok((&owned_children[..], buf)),
+                }
+                
+                for (byte, _exists) in child_exists.iter().enumerate().filter(|(_byte, exists)| *exists) {
+                    match Self::deserialize_next(buf) {
+                        Err(err) => return Err(err),
+                        Ok((child, remaining_buf)) => {
+                            next.children.insert(byte as u8, child);
+                            buf = remaining_buf;
+                        },
                     }
-                } else {
-                    Err("Not enough bytes".to_string())
                 }
-            } else if MAGIC_BYTE_CHILDREN_IS_BITMAP == magic_byte & MAGIC_BYTE_CHILDREN_IS_BITMAP {
-                if buf.len() < 32 {
-                    Err("Not enough bytes".to_string())
-                } else {
-                    Ok((buf[0..32].into(), &buf[32..]))
-                }
-            } else if MAGIC_BYTE_CHILDREN_IS_EMPTY == magic_byte & MAGIC_BYTE_CHILDREN_IS_EMPTY {
-                Ok((&owned_children[..], buf))
-            } else {
-                Err(format!("Invalid magic byte {}", magic_byte))
-            } {
-                Err(err) => return Err(err),
-                Ok((child_exists, mut buf)) => {
-                    for (byte, _exists) in child_exists.iter().enumerate().filter(|(_byte, exists)| *exists) {
-                        match Self::deserialize_next(buf) {
-                            Ok((child, remaining_buf)) => {
-                                next.children.insert(byte as u8, child);
-                                buf = remaining_buf;
-                            },
-                            Err(err) => return Err(err),
-                        }
-                    }
-                    return Ok((next, buf));
-                }
-            }
+                Ok((next, buf))
+            },
         }
-
-        return Err("Not enough bytes".to_string());
     }
 }
